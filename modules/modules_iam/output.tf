279b94d37168a19ec98f0831087612a763ed3230
daanviros <dvillarreal_@hotmail.com>
2024-06-07 14:17:57 -0600

    Carga de archivos del Laboratorio 1

---
+++ modules/modules_iam/output.tf
@@ -0,0 +1,8 @@
+output "sessionpolicy" {
+    value = aws_iam_policy.ssmpolicyec2.arn
+  
+}
+
+output "role-arn" {
+  value = aws_iam_instance_profile.ec2Profile.arn
+}
---
+++ modules/modules_iam/variables.tf
@@ -0,0 +1,9 @@
+variable "datapolicy" {
+  description = "Datos de la politica customer Managed"
+  type = map(string)
+}
+
+variable "datarole" {
+  description = "Datos de la Instancia Profile Role"
+  type = map(string)
+}
